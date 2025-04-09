a61df04ebc8d91bb26c3da4fa1bc1fc26389fe61
kota_suzuki <kota_suzuki@m-redp.local>
2025-04-09 18:56:29 +0900

    add: shachihoko and tokyo_tower

--- js/main.js
+++ js/main.js
@@ -20,91 +20,113 @@ window.GThrower = window.GThrower || {};
         // 画像ファイルのベースパス
         const iconsBasePath = 'Assets/images/icons/';
 
-        // 1. コロッセオ (Colosseum)
+        // 1. 清水寺 (Kiyomizu Temple)
         createIphoneLikeButton(
             containerId,
             '1', // ステージ番号
-            iconsBasePath + 'colosseum.png',
+            iconsBasePath + 'kiyomizu_temple.png',
             () => {
-                console.log('ステージ 1 開始！ (Colosseum)');
+                console.log('ステージ 1 開始！ (Kiyomizu Temple)');
                 // G.sceneManager.startGame(1); のような処理
             }
         );
 
-        // 2. エッフェル塔 (Eiffel Tower)
+        // 2. 鯱鉾 (Shachihoko)
         createIphoneLikeButton(
             containerId,
-            '2',
-            iconsBasePath + 'eiffel_tower.png',
+            '2', // ステージ番号
+            iconsBasePath + 'shachihoko.png',
             () => {
-                console.log('ステージ 2 開始！ (Eiffel Tower)');
+                console.log('ステージ 2 開始！ (Shachihoko)');
                 // G.sceneManager.startGame(2); のような処理
             }
         );
 
-        // 3. 五重塔 (Five-storied Pagoda)
+        // 3. 東京タワー (Tokyo Tower)
         createIphoneLikeButton(
             containerId,
-            '3',
-            iconsBasePath + 'Five-storied_pagoda.png',
+            '3', // ステージ番号
+            iconsBasePath + 'tokyo_tower.png',
             () => {
-                console.log('ステージ 3 開始！ (Five-storied Pagoda)');
+                console.log('ステージ 3 開始！ (Tokyo Tower)');
                 // G.sceneManager.startGame(3); のような処理
             }
         );
 
-        // 4. 清水寺 (Kiyomizu Temple)
+        // 4. 五重塔 (Five-storied Pagoda)
         createIphoneLikeButton(
             containerId,
-            '4',
-            iconsBasePath + 'kiyomizu_temple.png',
+            '4', // ステージ番号
+            iconsBasePath + 'Five-storied_pagoda.png',
             () => {
-                console.log('ステージ 4 開始！ (Kiyomizu Temple)');
+                console.log('ステージ 4 開始！ (Five-storied Pagoda)');
                 // G.sceneManager.startGame(4); のような処理
             }
         );
 
-        // 5. モアイ (Moai) - 例にもありましたがリストに含まれていたため再掲
+        // 5. 自由の女神 (Statue of Liberty)
         createIphoneLikeButton(
             containerId,
-            '5',
-            iconsBasePath + 'moai.png',
+            '5', // ステージ番号
+            iconsBasePath + 'statue_of_liberty.png',
             () => {
-                console.log('ステージ 5 開始！ (Moai)');
+                console.log('ステージ 5 開始！ (Statue of Liberty)');
                 // G.sceneManager.startGame(5); のような処理
             }
         );
 
-        // 6. モン・サン＝ミシェル (Mont Saint-Michel)
+        // 6. エッフェル塔 (Eiffel Tower)
         createIphoneLikeButton(
             containerId,
-            '6',
-            iconsBasePath + 'mont_saint-michel.png',
+            '6', // ステージ番号
+            iconsBasePath + 'eiffel_tower.png',
             () => {
-                console.log('ステージ 6 開始！ (Mont Saint-Michel)');
+                console.log('ステージ 6 開始！ (Eiffel Tower)');
                 // G.sceneManager.startGame(6); のような処理
             }
         );
 
-        // 7. スフィンクス (Sphinx)
+        // 7. コロッセオ (Colosseum)
         createIphoneLikeButton(
             containerId,
-            '7',
-            iconsBasePath + 'sphinx.png',
+            '7', // ステージ番号
+            iconsBasePath + 'colosseum.png',
             () => {
-                console.log('ステージ 7 開始！ (Sphinx)');
+                console.log('ステージ 7 開始！ (Colosseum)');
                 // G.sceneManager.startGame(7); のような処理
             }
         );
 
-        // 8. 自由の女神 (Statue of Liberty)
+        // 8. モアイ (Moai)
         createIphoneLikeButton(
             containerId,
-            '8',
-            iconsBasePath + 'statue_of_liberty.png',
+            '8', // ステージ番号
+            iconsBasePath + 'moai.png',
             () => {
-                console.log('ステージ 8 開始！ (Statue of Liberty)');
+                console.log('ステージ 8 開始！ (Moai)');
                 // G.sceneManager.startGame(8); のような処理
+            }
+        );
+
+        // 9. モン・サン＝ミシェル (Mont Saint-Michel)
+        createIphoneLikeButton(
+            containerId,
+            '9', // ステージ番号
+            iconsBasePath + 'mont_saint-michel.png',
+            () => {
+                console.log('ステージ 9 開始！ (Mont Saint-Michel)');
+                // G.sceneManager.startGame(9); のような処理
+            }
+        );
+
+        // 10. スフィンクス (Sphinx)
+        createIphoneLikeButton(
+            containerId,
+            '10', // ステージ番号
+            iconsBasePath + 'sphinx.png',
+            () => {
+                console.log('ステージ 10 開始！ (Sphinx)');
+                // G.sceneManager.startGame(10); のような処理
             }
         );
     }); // DOMContentLoaded の終わり
